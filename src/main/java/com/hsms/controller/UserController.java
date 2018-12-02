d79ffdda0c4f14f910c0ca71eaaeabf7f29d4e7c
jhone <jhonexiaomei@gmail.com>
2018-12-02 21:17:15 +0800

    删除无用类引用

--- src/main/java/com/hsms/controller/UserController.java
+++ src/main/java/com/hsms/controller/UserController.java
@@ -1,21 +1,15 @@
 package com.hsms.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.hsms.common.ResponseJsonPageListBean;
 import com.hsms.model.SysUser;
 import com.hsms.pojo.ResultPojo;
 import com.hsms.service.UsersService;
-import com.hsms.utils.JsonPrintUtil;
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion.User;
 
 @Controller
 @RequestMapping("/user")
